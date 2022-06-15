009060caca1aa0a6490248fd0e9811a795c724bf
HP <HP@DESKTOP-32NKH3D>
2022-06-15 15:17:30 +0530

    by shubham

--- Git2/src/Demo.java
+++ Git2/src/Demo.java
@@ -1,6 +1,13 @@
 
 public class Demo {
 
+	
+	public void m1()
+	{
+		System.out.println("m1--method");
+	}
+	
+	
 	public static void main(String[] args) {
 	
 		System.out.println("welcome to wardha");
